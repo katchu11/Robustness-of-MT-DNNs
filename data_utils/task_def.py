512761ffd9812bf2e57784acac5c6268a957bed6
Kashyap Murali <13315300+katchu11@users.noreply.github.com>
2019-12-13 23:21:05 -0500

    Add files via upload

---
+++ data_utils/task_def.py
@@ -0,0 +1,18 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+from enum import IntEnum
+class TaskType(IntEnum):
+ Classification = 1
+ Regression = 2
+ Ranking = 3
+
+
+class DataFormat(IntEnum):
+    PremiseOnly = 1
+    PremiseAndOneHypothesis = 2
+    PremiseAndMultiHypothesis = 3
+
+class EncoderModelType(IntEnum):
+    BERT = 1
+    ROBERTA = 2
+    XLNET = 3
